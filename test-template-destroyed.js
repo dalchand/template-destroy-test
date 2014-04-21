93b21399e38df3262c5b742c97e870383ba8db0e
Dalchand <dalchand@ltresearch.com>
2014-04-21 18:19:44 +0530

    no element creation

--- test-template-destroyed.js
+++ test-template-destroyed.js
@@ -3,9 +3,7 @@ if (Meteor.isClient) {
   UI.body.events({
     "click .render": function() {
       var el = $(".template").get(0);
-      var element = document.createElement("div");
-      el.appendChild(element);
-      UI.insert(UI.renderWithData(Template.hello, {name: "Dalchand"}), element);
+      UI.insert(UI.renderWithData(Template.hello, {name: "Dalchand"}), el);
     },
     "click .destroy": function() {
       var el = $(".template").get(0);
